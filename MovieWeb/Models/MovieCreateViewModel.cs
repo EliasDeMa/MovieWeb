ead83180c4041eb1960ffa22e352d7c9b9befc54
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-08 15:36:52 +0200

    Update displaynames.

--- MovieWeb/Models/MovieCreateViewModel.cs
+++ MovieWeb/Models/MovieCreateViewModel.cs
@@ -9,21 +9,16 @@ namespace MovieWeb.Models
 {
     public class MovieCreateViewModel
     {
-        [DisplayName("Titel")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Titel verplicht!")]
-        [MaxLength(30, ErrorMessage = "Maximum 30 karakters!")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Title required!")]
+        [MaxLength(30, ErrorMessage = "Maximum 30 characters!")]
         public string Title { get; set; }
 
-        [DisplayName("Omschrijving")]
-        [MaxLength(250, ErrorMessage = "Maximum 250 karakters!")]
+        [MaxLength(250, ErrorMessage = "Maximum 250 characters!")]
         public string Description { get; set; }
 
-        [DisplayName("Genre")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Genre verplicht!")]
-        [MaxLength(30, ErrorMessage = "Maximum 30 karakters!")]
+        [MaxLength(30, ErrorMessage = "Maximum 30 characters!")]
         public string Genre { get; set; }
 
-        [DisplayName("Release Datum")]
         [Range(typeof(DateTime), "1/1/1900", "1/1/2070")]
         public DateTime ReleaseDate { get; set; }
     }
