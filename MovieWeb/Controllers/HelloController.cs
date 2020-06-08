ac37c373a98781c43ad1443d032e5e246277884c
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-08 11:06:23 +0200

    Add project files.

---
+++ MovieWeb/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieWeb.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Welcome(string name, int numberOfTimes = 1)
+        {
+            ViewData["Name"] = name;
+            ViewData["NumberOfTimes"] = numberOfTimes;
+            return View();
+        }
+
+        public IActionResult Today()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieWeb/Controllers/MovieController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieWeb.Database;
+using MovieWeb.Domain;
+using MovieWeb.Models;
+
+namespace MovieWeb.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IMovieDatabase _movieDatabase;
+
+        public MovieController(IMovieDatabase movies)
+        {
+            _movieDatabase = movies;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = _movieDatabase.GetMovies()
+                .Select(item => new MovieIndexViewModel()
+                {
+                    Title = item.Title,
+                    Id = item.Id
+                })
+                .ToList();
+
+            return View(movies);
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie movieFromDb = _movieDatabase.GetMovie((int)id);
+
+            MovieDetailViewModel movie = new MovieDetailViewModel()
+            {
+                Title = movieFromDb.Title,
+                Genre = movieFromDb.Genre,
+                Description = movieFromDb.Description,
+                ReleaseDate = movieFromDb.ReleaseDate
+            };
+
+            return View(movie);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(MovieCreateViewModel movie)
+        {
+            _movieDatabase.Insert(new Movie()
+            {
+                Title = movie.Title,
+                Genre = movie.Genre,
+                Description = movie.Description,
+                ReleaseDate = movie.ReleaseDate
+            });
+
+            return RedirectToAction("Index");
+        }
+    }
+}
