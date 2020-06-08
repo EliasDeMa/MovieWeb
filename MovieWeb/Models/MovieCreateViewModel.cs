b8324539ad30f9370143bb8c54b6e1cde7de18da
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-08 12:49:55 +0200

    use validation in model

--- MovieWeb/Models/MovieCreateViewModel.cs
+++ MovieWeb/Models/MovieCreateViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,19 @@ namespace MovieWeb.Models
 {
     public class MovieCreateViewModel
     {
+        [DisplayName("Titel")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Titel verplicht!")]
+        [MaxLength(20, ErrorMessage = "Maximum 20 karakters!")]
         public string Title { get; set; }
+
+        [DisplayName("Omschrijving")]
         public string Description { get; set; }
+
+        [DisplayName("Genre")]
         public string Genre { get; set; }
+
+        [DisplayName("Release Datum")]
+        [DataType(DataType.DateTime)]
         public DateTime ReleaseDate { get; set; }
     }
 }
